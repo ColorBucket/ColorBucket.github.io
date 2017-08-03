cd2bc511534f63f75d53e4222782a59ab572e1cb
Gabriel Age <0800.grc@gmail.com>
2017-08-03 16:26:45 -0300

    Adding home alerts

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 //
 import Fab from '../fab/fab';
 import ColorTile from '../color/tile/color-tile';
@@ -29,8 +30,33 @@ class Home extends Component {
 	};
 
   render() {
+
+  	let homeMessage = () => {
+  		var closeAction = (e, chrome) => {
+  			e.target.parentElement.remove();
+  			if(chrome)
+  				localStorage.chromealert = !localStorage.chromealert ? 1 : 2;
+  		};
+
+  		if(this.state.colors.length === 0)
+  			return false;
+
+  		if(!localStorage.userToken)
+  			return (<p className="home-message">
+					Hey! You're not logged in. Make sure to <Link to="/login">login</Link> or <Link to="/login/signup">signup</Link> so you never lose your colors! 
+					<span onClick={(e) => closeAction(e)}>close</span>
+				</p>);
+
+  		if(!localStorage.chromealert || localStorage.chromealert != "2")
+  			return (<p className="home-message">
+  				Hey! Have you tried our <i>awesome</i> <a href="https://github.com/Colorbucket/chrome-extension" target="_blank">chrome extension</a>? 
+  				<span onClick={(e) => closeAction(e, true)}>close</span>
+				</p>);
+  	};
+
     return (
       <div className="home-page">
+      	{homeMessage()}
       	<div className="color-list">
 	        {this.state.colors.length === 0 ?
 	        	<p className="empty-list">save the colors that you ♥ so you'll never lose that nice color again</p>
