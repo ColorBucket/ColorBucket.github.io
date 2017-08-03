dcde20d3e24dd6b5bc2a7bc5efb5cf3ddae6508a
Gabriel Age <0800.grc@gmail.com>
2017-08-03 15:32:41 -0300

    Better looking temp about page

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -4,8 +4,9 @@ import React, { Component } from 'react';
 class About extends Component {
   render() {
     return (
-      <div>
-        about
+      <div className="col-12 center-text">
+        <h5 className="ma-40 ma-cl-h ma-cl-b">New about page coming soon.</h5>
+        <p>Until there, check out our github for more info at <a href="https://github.com/ColorBucket/" target="_blank">https://github.com/ColorBucket/</a></p>
       </div>
     );
   }
