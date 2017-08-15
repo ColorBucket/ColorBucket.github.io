3fb3bd554b646de321233b194f50dc43ec78e464
Gabriel Age <0800.grc@gmail.com>
2017-08-15 00:13:43 -0300

    Changing profile container to -lg

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -34,7 +34,7 @@ class Profile extends Component {
 
   render() {
     return (
-      <div className="container">
+      <div className="container-lg">
         { !this.state.user ?
           <p className="center-text">Whoops! User not found :-(</p>
           :
